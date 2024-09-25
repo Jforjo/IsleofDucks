2b8667d5ea44cf47d37b06bbfd9c4aee9342a1ae
Jforjo <jforjoooooo13@gmail.com>
2024-09-25 16:14:41 +0100

    .

--- api/commands.js
+++ api/commands.js
@@ -12,9 +12,9 @@ import { CommandData } from "../commands/application/checkapi";
 
 
 export default async (req, res) => {
-    // await InstallGlobalCommands([CommandData]);
+    await InstallGlobalCommands([CommandData]);
     // await InstallGuildCommands("997893922607087636", [CommandData]);
-    await DeleteGlobalCommands([{ id: "1288515773488435331" }, { id: "1288515771768504383" }]);
-    await DeleteGuildCommands("997893922607087636", [{ id: "1288515773488435331" }, { id: "1288515771768504383" }]);
+    // await DeleteGlobalCommands([{ id: "1288515773488435331" }, { id: "1288515771768504383" }]);
+    // await DeleteGuildCommands("997893922607087636", [{ id: "1288515773488435331" }, { id: "1288515771768504383" }]);
     return res.json({ message: "Commands Loaded" });
 }
