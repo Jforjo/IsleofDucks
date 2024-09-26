64349443abcb3e52ce940408382399d6f1572b66
Jforjo <jforjoooooo13@gmail.com>
2024-09-26 09:57:11 +0100

    .

--- api/commands.js
+++ api/commands.js
@@ -1,5 +1,6 @@
 import { InstallGlobalCommands, InstallGuildCommands, DeleteGlobalCommands, DeleteGuildCommands } from "../utils/discordUtils";
-import { CommandData } from "../commands/application/guildcata";
+import { CommandData as CheckAPI } from "../commands/application/checkapi";
+import { CommandData as GuildCata } from "../commands/application/guildcata";
 // COMMANDS.push({
 //     name: 'support',
 //     description: 'Like this bot? Support me!',
@@ -13,8 +14,8 @@ import { CommandData } from "../commands/application/guildcata";
 
 export default async (req, res) => {
     // await InstallGlobalCommands([CommandData]);
-    await InstallGuildCommands("997893922607087636", [CommandData]);
+    // await InstallGuildCommands("997893922607087636", [CheckAPI, GuildCata]);
     // await DeleteGlobalCommands([{ id: "" }]);
-    // await DeleteGuildCommands("997893922607087636", [{ id: "1288515773488435331" }, { id: "1288515771768504383" }]);
+    await DeleteGuildCommands("997893922607087636", [{ id: "1288783738511163424" }]);
     return res.json({ message: "Commands Loaded" });
 }
