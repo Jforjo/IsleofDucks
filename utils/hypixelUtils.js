2b683117e772aa9a92a3a34da8ab800b6ff2c1d1
Jforjo <jforjoooooo13@gmail.com>
2024-09-26 13:19:55 +0100

    .

--- utils/hypixelUtils.js
+++ utils/hypixelUtils.js
@@ -3,10 +3,9 @@ export async function getUUID(username) {
         mode: 'no-cors'
     });
     if (!res.ok) {
-        console.log("hypixelUtils.js - getUUID: " + res);
         return {
             success: false,
-            message: 'Bad response from Mojang (UUID)'
+            message: 'Bad response from Mojang'
         };
     }
     const data = await res.json();
@@ -20,23 +19,44 @@ export async function getUUID(username) {
         name: data.name
     };
 }
+// export async function getUsername(uuid) {
+//     const res = await fetch(`https://sessionserver.mojang.com/session/minecraft/profile/${uuid}`, {
+//         mode: 'no-cors',
+//         method: 'GET'
+//     });
+//     if (!res.ok) {
+//         const data = await res.json();
+//         console.log("hypixelUtils.js - getUsername: " + JSON.stringify(data));
+//         return {
+//             success: false,
+//             message: 'Bad response from Mojang (Username)\n' + uuid + '\n' + JSON.stringify(data)
+//         };
+//     }
+//     const data = await res.json();
+//     if (data.id === null) return {
+//         success: false,
+//         message: data?.errorMessage ? data.errorMessage : 'UUID not found'
+//     }
+//     return {
+//         success: true,
+//         uuid: data.id,
+//         name: data.name
+//     };
+// }
 export async function getUsername(uuid) {
-    const res = await fetch(`https://sessionserver.mojang.com/session/minecraft/profile/${uuid}`, {
-        mode: 'no-cors',
-        method: 'GET'
+    const res = await fetch(`https://api.minetools.eu/uuid/${encodeURIComponent(uuid)}`, {
+        mode: 'no-cors'
     });
     if (!res.ok) {
-        const data = await res.json();
-        console.log("hypixelUtils.js - getUsername: " + JSON.stringify(data));
         return {
             success: false,
-            message: 'Bad response from Mojang (Username)\n' + uuid + '\n' + JSON.stringify(data)
+            message: 'Bad response from Minetools'
         };
     }
     const data = await res.json();
     if (data.id === null) return {
         success: false,
-        message: data?.errorMessage ? data.errorMessage : 'UUID not found'
+        message: data?.error ? data.error : 'UUID not found'
     }
     return {
         success: true,
