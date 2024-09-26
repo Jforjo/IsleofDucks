9c05c7cfeb766d39963e8ea9bb34914a21aa55a4
Jforjo <jforjoooooo13@gmail.com>
2024-09-26 15:08:19 +0100

    .

--- commands/application/guildcata.js
+++ commands/application/guildcata.js
@@ -3,7 +3,7 @@ import { InteractionResponseType } from "discord-interactions";
 import { getUUID, getUsername } from "../../utils/hypixelUtils";
 import { CreateInteractionResponse, FollowupMessage } from "../../utils/discordUtils";
 
-const catalevels = { 0: 0,
+const catalevels = {
     1: 50, 2: 125, 3: 235, 4: 395, 5: 625, 6: 955, 7: 1425, 8: 2095, 9: 3045,
     10: 4385, 11: 6275, 12: 8940, 13: 12700, 14: 17960, 15: 25340, 16: 35640,
     17: 50040, 18: 70040, 19: 97640, 20: 135640, 21: 188140, 22: 259640, 23: 356640,
@@ -11,9 +11,18 @@ const catalevels = { 0: 0,
     30: 3084640, 31: 4149640, 32: 5559640, 33: 7459640, 34: 9959640, 35: 13259640,
     36: 17559640, 37: 23159640, 38: 30359640, 39: 39559640, 40: 51559640, 41: 66559640,
     42: 85559640, 43: 109559640, 44: 139559640, 45: 177559640, 46: 225559640,
-    47: 285559640, 48: 360559640, 49: 453559640, 50: 569809640
-};
-
+    47: 285559640, 48: 360559640, 49: 453559640, 50: 569809640, 51: 769809640,
+    52: 967809640, 53: 1167809640, 54: 1367809640, 55: 1567809640, 56: 1767809640,
+    57: 1967809640, 58: 2167809640, 59: 2367809640, 60: 2567809640, 61: 2767809640,
+    62: 2967809640, 63: 3167809640, 64: 3367809640, 65: 3567809640, 66: 3767809640,
+    67: 3967809640, 68: 4167809640, 69: 4367809640, 70: 4567809640, 71: 4767809640,
+    72: 4967809640, 73: 5167809640, 74: 5367809640, 75: 5567809640, 76: 5767809640,
+    77: 5967809640, 78: 6167809640, 79: 6367809640, 80: 6567809640, 81: 6767809640,
+    82: 6967809640, 83: 7167809640, 84: 7367809640, 85: 7567809640, 86: 7767809640,
+    87: 7967809640, 88: 8167809640, 89: 8367809640, 90: 8567809640, 91: 8767809640,
+    92: 8967809640, 93: 9167809640, 94: 9367809640, 95: 9567809640, 96: 9767809640,
+    97: 9967809640, 98: 10167809640, 99: 10367809640, 100: 10567809640,
+}
 async function getCurrentCataLevel(uuid) {
     const response = await fetch(`https://api.hypixel.net/v2/skyblock/profiles?uuid=${encodeURIComponent(uuid)}`, {
         method: 'GET',
@@ -48,10 +57,10 @@ async function getCurrentCataLevel(uuid) {
         }
     });
 
-    let catalvl = -1;
+    let catalvl = 0;
     for (const [key, value] of Object.entries(catalevels)) {
         if (cataexp < value) {
-            catalvl += (cataexp - catalevels[key - 1]) / (value - catalevels[key - 1]);
+            catalvl += (cataexp - (catalevels[key - 1] ?? 0)) / (value - (catalevels[key - 1] ?? 0));
             break;
         }
         catalvl++;
@@ -166,7 +175,7 @@ export default async (req, res) => {
         fieldArray.push(
             {
                 name: '\u200b',
-                value: result.slice(i, i + chunkSize).map((field) => `\`${field.rank}\` ${field.name}: ${Math.round((field.cataLevel + Number.EPSILON) * 100) / 100}`).join('\n'),
+                value: result.slice(i, i + chunkSize).map((field) => `\`#${field.rank}\` ${field.name.replace('_', '\_')}: ${Math.round((field.cataLevel + Number.EPSILON) * 100) / 100}`).join('\n'),
                 inline: true
             }
         );
