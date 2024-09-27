bacb2668553a703b6cd53030fb546cea170a5a0f
Jforjo <jforjoooooo13@gmail.com>
2024-09-27 13:45:24 +0100

    .

--- api/commands.js
+++ api/commands.js
@@ -14,9 +14,9 @@ import { CommandData as Superlative } from "../commands/application/superlative.
 
 
 export default async (req, res) => {
-    // const result = await InstallGlobalCommands([CheckAPI]);
+    const result = await InstallGlobalCommands([CheckAPI]);
     // const result = await InstallGuildCommands("997893922607087636", [CheckAPI, GuildCata]);
     // await DeleteGlobalCommands([{ id: "" }]);
-    await DeleteGuildCommands("997893922607087636", [{ id: "1288791933543649432" }]);
-    // return res.json({ message: result });
+    // await DeleteGuildCommands("997893922607087636", [{ id: "1288791933543649432" }]);
+    return res.json({ message: result });
 }
