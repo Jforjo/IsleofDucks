7f1360640c0b414b8e8e767d9910e08a7a2a2e70
Jforjo <jforjoooooo13@gmail.com>
2024-09-27 13:25:23 +0100

    .

--- api/commands.js
+++ api/commands.js
@@ -1,6 +1,7 @@
 import { InstallGlobalCommands, InstallGuildCommands, DeleteGlobalCommands, DeleteGuildCommands } from "../utils/discordUtils";
-import { CommandData as CheckAPI } from "../commands/application/checkapi";
-import { CommandData as GuildCata } from "../commands/application/guildcata";
+import { CommandData as CheckAPI } from "../commands/application/checkapi.js";
+import { CommandData as GuildCata } from "../commands/application/guildcata.js";
+import { CommandData as Superlative } from "../commands/application/superlative.js";
 // COMMANDS.push({
 //     name: 'support',
 //     description: 'Like this bot? Support me!',
@@ -13,7 +14,7 @@ import { CommandData as GuildCata } from "../commands/application/guildcata";
 
 
 export default async (req, res) => {
-    // await InstallGlobalCommands([CommandData]);
+    await InstallGlobalCommands([Superlative]);
     // const result = await InstallGuildCommands("997893922607087636", [CheckAPI, GuildCata]);
     // await DeleteGlobalCommands([{ id: "" }]);
     // await DeleteGuildCommands("997893922607087636", [{ id: "1288787008252018711" }, { id: "1288787008252018710" }]);
