9886d0db6feaa6b435c5ad91a016d1ccccf6f45b
Jforjo <jforjoooooo13@gmail.com>
2024-09-27 18:23:42 +0100

    .

--- commands/application/ping.js
+++ commands/application/ping.js
@@ -15,7 +15,7 @@ export default async (req, res) => {
                     title: "Pong!",
                     color: parseInt("FF69B4", 16),
                     footer: {
-                        text: `Response time: ${date.getTime() - timestamp}ms`,
+                        text: `Response time: ${interaction.id}ms`,
                     },
                     timestamp: date.toISOString()
                 }
