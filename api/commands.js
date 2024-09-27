2f924b194b7a9494cf19342448e8a435e54088e6
Jforjo <jforjoooooo13@gmail.com>
2024-09-27 17:19:38 +0100

    .

--- api/commands.js
+++ api/commands.js
@@ -2,6 +2,7 @@ import { InstallGlobalCommands, InstallGuildCommands, DeleteGlobalCommands, Dele
 import checkapi, { CommandData as CheckAPI } from "../commands/application/checkapi.js";
 import { CommandData as GuildCata } from "../commands/application/guildcata.js";
 import { CommandData as Superlative } from "../commands/application/superlative.js";
+import { CommandData as Ping } from "../commands/application/ping.js";
 // COMMANDS.push({
 //     name: 'support',
 //     description: 'Like this bot? Support me!',
@@ -15,9 +16,9 @@ import { CommandData as Superlative } from "../commands/application/superlative.
 
 export default async (req, res) => {
     // const result = await InstallGlobalCommands([CheckAPI, Superlative]);
-    // const result = await InstallGuildCommands("997893922607087636", [CheckAPI, GuildCata]);
+    const result = await InstallGuildCommands("997893922607087636", [Ping]);
     // await DeleteGlobalCommands([{ id: "" }]);
     // await DeleteGuildCommands("997893922607087636", [{ id: "1288791933543649432" }]);
-    // return res.json({ message: result });
-    return res.json({ message: "Hello, World!" });
+    return res.json({ message: result });
+    // return res.json({ message: "Hello, World!" });
 }
---
+++ commands/application/ping.js
@@ -0,0 +1,31 @@
+import { InteractionResponseType } from "discord-interactions";
+
+export default async (req, res) => {
+    const interaction = req.body;
+    const date = new Date();
+    return res.status(200).send({
+        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        data: {
+            content: `
+                \`\`\`json
+                ${JSON.stringify(interaction, null, 4)}
+                \`\`\`
+            `,
+            embeds: [
+                {
+                    title: "Pong!",
+                    color: parseInt("FF69B4", 16),
+                    // footer: {
+                    //     text: `Response time: ${date.getTime() - interaction.timestamp}ms`,
+                    // },
+                    timestamp: date.toISOString()
+                }
+            ],
+        },
+    });
+}
+export const CommandData = {
+    name: "ping",
+    description: "Pings the bot!",
+    type: ApplicationCommandType.ChatInput,
+}
