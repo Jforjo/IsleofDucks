887a048730de3280021f9182c36a89213c254772
Jforjo <jforjoooooo13@gmail.com>
2024-09-27 17:52:08 +0100

    .

--- commands/application/ping.js
+++ commands/application/ping.js
@@ -4,6 +4,8 @@ import { ApplicationCommandOptionType, ApplicationCommandType } from "discord-ap
 export default async (req, res) => {
     const interaction = req.body;
     const date = new Date();
+    const DISCORD_EPOCH = 14200070400000;
+    const timestamp = parseInt(interaction.id.slice(0,42), 2) + DISCORD_EPOCH;
     return res.status(200).send({
         type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
         data: {
@@ -13,7 +15,7 @@ export default async (req, res) => {
                     title: "Pong!",
                     color: parseInt("FF69B4", 16),
                     footer: {
-                        text: `Response time: ${Date.now() - interaction.createdTimestamp}ms`,
+                        text: `Response time: ${Date.now() - timestamp}ms`,
                     },
                     timestamp: date.toISOString()
                 }
