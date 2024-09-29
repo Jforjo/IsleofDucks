571f5078bb6474b69e23d46cb83515973abe8ec8
Jforjo <jforjoooooo13@gmail.com>
2024-09-29 07:24:30 +0100

    .

--- api/commands.js
+++ api/commands.js
@@ -1,8 +1,9 @@
 import { InstallGlobalCommands, InstallGuildCommands, DeleteGlobalCommands, DeleteGuildCommands } from "../utils/discordUtils.js";
 import checkapi, { CommandData as CheckAPI } from "../commands/application/checkapi.js";
-import { CommandData as GuildCata } from "../commands/application/guildcata.js";
+import guildcata, { CommandData as GuildCata } from "../commands/application/guildcata.js";
 import { CommandData as Superlative } from "../commands/application/superlative.js";
 import { CommandData as Ping } from "../commands/application/ping.js";
+import { CommandData as Test } from "../commands/application/test.js";
 // COMMANDS.push({
 //     name: 'support',
 //     description: 'Like this bot? Support me!',
@@ -16,9 +17,9 @@ import { CommandData as Ping } from "../commands/application/ping.js";
 
 export default async (req, res) => {
     // const result = await InstallGlobalCommands([CheckAPI, Superlative]);
-    // const result = await InstallGuildCommands("997893922607087636", [Ping]);
+    const result = await InstallGuildCommands("997893922607087636", [Test, GuildCata]);
     // await DeleteGlobalCommands([{ id: "" }]);
     // await DeleteGuildCommands("997893922607087636", [{ id: "1288791933543649432" }]);
-    // return res.json({ message: result });
-    return res.json({ message: "Hello, World!" });
+    return res.json({ message: result });
+    // return res.json({ message: "Hello, World!" });
 }
