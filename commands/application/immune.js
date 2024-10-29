4aee76151a9f9b8d5a2c18c3bb3ca508440da275
Jforjo <jforjoooooo13@gmail.com>
2024-10-29 16:24:18 +0000

    Added command to return embed of all immune players

---
+++ commands/application/immune.js
@@ -0,0 +1,76 @@
+import { ApplicationCommandType } from "discord-api-types/v10";
+import { InteractionResponseType } from "discord-interactions";
+import { getUsername } from "../../utils/hypixelUtils.js";
+import { CreateInteractionResponse, ConvertSnowflakeToDate, FollowupMessage } from "../../utils/discordUtils.js";
+
+async function getImmunePlayers() {
+    const res = await fetch('/api/immune');
+    const data = await res.json();
+    if (!res.ok) {
+        return {
+            success: false,
+            message: 'Bad response',
+            ping: false
+        };
+    }
+    const players = [];
+    data.forEach(id => {
+        const username = getUsername(id);
+        if (username.success) {
+            players.push({
+                id: id, name: username.name
+            });
+        }
+    });
+    return players;
+}
+
+export default async (req, res) => {
+    const interaction = req.body;
+
+    // User sees the "[bot] is thinking..." message
+    await CreateInteractionResponse(interaction.id, interaction.token, {
+        type: InteractionResponseType.DEFERRED_CHANNEL_MESSAGE_WITH_SOURCE,
+    });
+
+    const timestamp = ConvertSnowflakeToDate(interaction.id);
+
+    const immunePlayers = await getImmunePlayers();
+
+    if (!immunePlayers.success) {
+        return await FollowupMessage(interaction.token, {
+            content: null,
+            embeds: [
+                {
+                    title: "Something went wrong!",
+                    description: immunePlayers.message,
+                    color: parseInt("B00020", 16),
+                    footer: {
+                        text: `Response time: ${Date.now() - timestamp.getTime()}ms`,
+                    },
+                    timestamp: new Date().toISOString()
+                }
+            ],
+        });
+    }
+
+    return await FollowupMessage(interaction.token, {
+        content: null,
+        embeds: [
+            {
+                title: "Immune Players",
+                description: immunePlayers.map(player => player.name).join('\n'),
+                color: parseInt("FB9B00", 16),
+                footer: {
+                    text: `Response time: ${Date.now() - timestamp.getTime()}ms`,
+                },
+                timestamp: new Date().toISOString()
+            }
+        ],
+    });
+}
+export const CommandData = {
+    name: "immune",
+    description: "Returns a list of all immune players.",
+    type: ApplicationCommandType.ChatInput,
+}
