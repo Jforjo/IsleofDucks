401a82f132ef4f08e194469ca6d2f3756135fe3f
Jforjo <jforjoooooo13@gmail.com>
2024-12-01 07:58:02 +0000

    update: show old superlative once when new one is in place

--- commands/application/superlative.js
+++ commands/application/superlative.js
@@ -28,6 +28,10 @@ async function getSuperlative() {
             if (superlative.id !== currentSuperlative) {
                 await sql`UPDATE settings SET value = ${superlative.id} WHERE key = 'superlative'`;
                 await sql`UPDATE settings SET value = ${"true"} WHERE key = 'superlativeReset'`;
+                // Return the previous superlative to get the final results
+                if (superlative[i + 1] != null) {
+                    return superlative[i + 1];
+                }
             }
             return superlative;
         }
