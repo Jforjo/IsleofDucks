2aeb1729a6d1f5988ff187d4c9cfdfd5e95bf8d6
Jforjo <jforjoooooo13@gmail.com>
2024-12-04 10:13:12 +0000

    update: crud functions for immune players

---
+++ src/discord/utils.ts
@@ -0,0 +1,41 @@
+import { sql } from '@vercel/postgres';
+import { getUsernameOrUUID } from './hypixelUtils.js';
+
+export async function getImmunePlayers(): Promise<{
+    success: boolean;
+    players: Array<{
+        uuid: string;
+        name?: string;
+        discord: string;
+        reason: string;
+    }>;
+}> {
+    const { rows } = await sql`SELECT uuid, discord, reason FROM immune`;
+
+    const players = await Promise.all(rows.map(async (row) => {
+        const nameRes = await getUsernameOrUUID(row.uuid);
+        let name = undefined;
+        if (nameRes.success === true) name = nameRes.name;
+        return {
+            uuid: row.uuid,
+            name: name,
+            discord: row.discord,
+            reason: row.reason
+        }
+    }));
+
+    return {
+        success: true,
+        players: players
+    };
+}
+export async function isImmunePlayer(uuid: string): Promise<boolean> {
+    const { rows } = await sql`SELECT * FROM immune WHERE uuid = ${uuid}`;
+    return rows.length > 0;
+}
+export async function addImmunePlayer(uuid: string, discord: string | null, reason: string): Promise<void> {
+    await sql`INSERT INTO immune (uuid, discord, reason) VALUES (${uuid}, ${discord}, ${reason})`;
+}
+export async function removeImmunePlayer(uuid: string): Promise<void> {
+    await sql`DELETE FROM immune WHERE uuid = ${uuid}`;
+}
