dc4853700905e6f6cace2bf0983513d00814ed9f
Jforjo <jforjoooooo13@gmail.com>
2024-12-08 11:54:10 +0000

    fix

--- src/app/(admin)/_components/header.tsx
+++ src/app/(admin)/_components/header.tsx
@@ -1,13 +1,10 @@
 "use client";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 import React from "react";
 import DiscordLogin from "@/components/ui/login";
 import HoverImage from "@/components/ui/hoverImage";
 
 export default function Header(): React.JSX.Element {
-    const pathname = usePathname();
-    
     return (
         <header className="relative flex justify-between items-center p-3 gap-6 dark:bg-neutral-900">
             <Link href="/" className="text-2xl font-bold text-nowrap flex items-center gap-3">
