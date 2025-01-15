2821654592cf9451d85f02a606bd2c78dc2b718f
Jforjo <jforjoooooo13@gmail.com>
2025-01-15 11:16:27 +0000

    fix: fixed regex

--- src/discord/tickets/claim.ts
+++ src/discord/tickets/claim.ts
@@ -22,7 +22,7 @@ export async function CheckGiveaways(
 
     for (const message of messagesToCheck) {
         // Backslash: .*+?^=!:${}()|[]/\
-        const newWinnerCheck = /The new winner of the giveaway (.*) is \<\@(.*)\>\. Congrats\!/gm.exec(message.content);
+        const newWinnerCheck = /The new winner of the giveaway (.*) is <@(.*)>. Congrats!/gm.exec(message.content);
         if (newWinnerCheck) {
             const title = newWinnerCheck[1];
             const winner = newWinnerCheck[2];
@@ -35,7 +35,7 @@ export async function CheckGiveaways(
             }
         }
         if (!message.embeds[0].description) continue;
-        const winnerCheck = /\<\@(.*)\> won the giveaway of \[(.*)\]/gm.exec(message.embeds[0].description);
+        const winnerCheck = /<@(.*)> won the giveaway of \[(.*)\]/gm.exec(message.embeds[0].description);
         if (winnerCheck) {
             const winner = winnerCheck[1];
             const title = winnerCheck[2];
