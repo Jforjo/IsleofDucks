a96c70b22d9182ff58ac50b69723d07813c2b18e
Jforjo <jforjoooooo13@gmail.com>
2025-05-09 08:32:27 +0100

    .

--- src/discord/commands/application/test.ts
+++ src/discord/commands/application/test.ts
@@ -1,9 +1,10 @@
 import { CreateInteractionResponse, FollowupMessage, IsleofDucks, SendMessage } from "@/discord/discordUtils";
-import { getUsernameOrUUID } from "@/discord/hypixelUtils";
-import { getDiscordRole } from "@/discord/utils";
-import { sql } from "@vercel/postgres";
+// import { getUsernameOrUUID } from "@/discord/hypixelUtils";
+// import { getDiscordRole } from "@/discord/utils";
+// import { sql } from "@vercel/postgres";
 import { APIChatInputApplicationCommandInteraction, APIInteractionResponse, ApplicationCommandType, InteractionResponseType, MessageFlags, RESTPatchAPIApplicationCommandJSONBody } from "discord-api-types/v10";
 import { NextResponse } from "next/server";
+import { UpdateAllDiscordIDsInDb } from "./updatedatabase";
 
 export default async function(
     interaction: APIChatInputApplicationCommandInteraction
@@ -52,25 +53,28 @@ export default async function(
             flags: MessageFlags.Ephemeral
         }
     });
+
+    await UpdateAllDiscordIDsInDb();
     
-    const { rows } = await sql`SELECT * FROM users`;
+    // const { rows } = await sql`SELECT * FROM users`;
 
-    const users = await Promise.all(rows.map(async (user) => {
-        const res = await getUsernameOrUUID(user.uuid);
-        const disc = await getDiscordRole(user.uuid);
-        return {
-            uuid: user.uuid,
-            name: res.success ? res.name : user.uuid,
-            disc: disc?.discordid
-        }
-    }));
+    // const users = await Promise.all(rows.map(async (user) => {
+    //     const res = await getUsernameOrUUID(user.uuid);
+    //     const disc = await getDiscordRole(user.uuid);
+    //     return {
+    //         uuid: user.uuid,
+    //         name: res.success ? res.name : user.uuid,
+    //         disc: disc?.discordid
+    //     }
+    // }));
 
-    const chunkSize = 20;
-    for (let i = 0; i < users.length; i += chunkSize) {
-        await SendMessage(interaction.channel.id, {
-            content: users.slice(i, i + chunkSize).map((user) => `\`${user.name}\` (${user.uuid}) ${user.disc ? `<@${user.disc}>` : ""}`).join("\n"),
-        });
-    }
+    // const chunkSize = 20;
+    // for (let i = 0; i < users.length; i += chunkSize) {
+    //     await SendMessage(interaction.channel.id, {
+    //         content: users.slice(i, i + chunkSize).map((user) => `\`${user.name}\` (${user.uuid}) ${user.disc ? `<@${user.disc}>` : ""}`).join("\n"),
+    //         flags: MessageFlags.SuppressNotifications
+    //     });
+    // }
 
     // await CreateInteractionResponse(interaction.id, interaction.token, {
     //     type: InteractionResponseType.ChannelMessageWithSource,
