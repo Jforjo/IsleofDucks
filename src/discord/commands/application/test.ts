b2b16efa4fc7fdc13188b13a47ff86fb923b5034
Jforjo <jforjoooooo13@gmail.com>
2025-05-15 09:59:36 +0100

    .

--- src/discord/commands/application/test.ts
+++ src/discord/commands/application/test.ts
@@ -50,7 +50,7 @@ export default async function(
         }
     });
 
-    await SendMessage(interaction.token, {
+    await SendMessage(interaction.channel.id, {
         flags: MessageFlags.IsComponentsV2,
         components: [
             {
