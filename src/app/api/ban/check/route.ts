a01bdecbeb5f398953684d2e06606e3d9ff4182d
Jforjo <jforjoooooo13@gmail.com>
2025-07-23 12:59:24 +0100

    GET endpoints to check banned player auto adds if in scammer and not banlist

--- src/app/api/ban/check/route.ts
+++ src/app/api/ban/check/route.ts
@@ -1,6 +1,6 @@
 import { addDashesToUUID } from "@/discord/hypixelUtils";
 import { getScammerFromUUID } from "@/discord/jerry";
-import { getBannedPlayer } from "@/discord/utils";
+import { addBannedPlayer, getBannedPlayer, updateBannedPlayerDiscord } from "@/discord/utils";
 import type { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest): Promise<Response> {
@@ -31,11 +31,29 @@ export async function GET(request: NextRequest): Promise<Response> {
     }
     
     const bannedResponse = await getBannedPlayer(uuid);
+    if (bannedResponse) return Response.json({
+        success: true,
+        banned: true,
+        reason: bannedResponse.reason
+    });
     const scammerResponse = await getScammerFromUUID(addDashesToUUID(uuid.replaceAll('-', '')));
+    if (scammerResponse.success && scammerResponse.scammer) {
+        const discordIds = scammerResponse.details?.discordIds;
+        await addBannedPlayer(uuid, discordIds ? discordIds[0] : null, scammerResponse.details ? scammerResponse.details.reason : "Unknown");
+        if (discordIds && discordIds.length > 1) {
+            for (const discord of discordIds.slice(1)) {
+                await updateBannedPlayerDiscord(uuid, discord);
+            }
+        }
+        return Response.json({
+            success: true,
+            banned: true,
+            reason: scammerResponse.details ? scammerResponse.details.reason : "Unknown"
+        });
+    }
 
     return Response.json({
         success: true,
-        banned: bannedResponse,
-        scammer: scammerResponse
+        banned: false
     });
 }
