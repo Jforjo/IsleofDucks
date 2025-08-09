bfe61766a663c9a0c0c5e6cadcdd8baccec8450e
Jforjo <jforjoooooo13@gmail.com>
2025-08-09 06:49:37 +0100

    Center main content in RootLayout and adjust TitleGeneratorPage section layout

--- src/app/(public)/title-generator/page.tsx
+++ src/app/(public)/title-generator/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function TitleGeneratorPage(): React.JSX.Element {
     return (
-        <section className="flex flex-col p-4 gap-4 items-center">
+        <section className="flex grow flex-col p-4 gap-4 items-center">
             <TitleGenerator />
         </section>
     )
