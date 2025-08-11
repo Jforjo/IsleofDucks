1ba475f205d59edb36c6cd022c740e787cf116f3
Jforjo <jforjoooooo13@gmail.com>
2025-08-11 19:42:21 +0100

    Add emoji management commands: add, remove, and view emojis in the bridge

--- src/discord/commands/application/bridge.ts
+++ src/discord/commands/application/bridge.ts
@@ -1,7 +1,7 @@
 import { APIChatInputApplicationCommandInteraction, APIChatInputApplicationCommandInteractionData, APIInteractionResponse, ApplicationCommandOptionType, ButtonStyle, ComponentType, InteractionResponseType } from "discord-api-types/v10";
 import { CreateInteractionResponse, ConvertSnowflakeToDate, FollowupMessage, IsleofDucks } from "@/discord/discordUtils";
 import { NextResponse } from "next/server";
-import { addBridgeFilter, checkBridgeFilter, getBridgeFilters, getTotalBridgeFilters, removeBridgeFilter } from "@/discord/utils";
+import { addBridgeFilter, checkBridgeFilter, checkEmoji, addEmoji, getBridgeFilters, getTotalBridgeFilters, removeBridgeFilter, removeEmoji, getEmojis, getTotalEmojis } from "@/discord/utils";
 
 async function addFilter(
     interaction: APIChatInputApplicationCommandInteraction,
@@ -99,7 +99,6 @@ async function addFilter(
         { status: 200 }
     );
 }
-
 async function removeFilter(
     interaction: APIChatInputApplicationCommandInteraction,
     replace: string
@@ -195,7 +194,6 @@ async function removeFilter(
         { status: 200 }
     );
 }
-
 async function viewFilters(
     interaction: APIChatInputApplicationCommandInteraction
 ): Promise<
@@ -334,6 +332,359 @@ async function viewFilters(
     );
 }
 
+async function addChatEmoji(
+    interaction: APIChatInputApplicationCommandInteraction,
+    replace: string,
+    withText: string
+): Promise<
+    NextResponse<
+        {
+            success: boolean;
+            error?: string;
+        } | APIInteractionResponse
+    >
+> {
+    const timestamp = ConvertSnowflakeToDate(interaction.id);
+
+    if (!interaction.member) {
+        await FollowupMessage(interaction.token, {
+            embeds: [
+                {
+                    title: "Something went wrong!",
+                    description: "Could not find who ran the command",
+                    color: IsleofDucks.colours.error,
+                    footer: {
+                        text: `Response time: ${Date.now() - timestamp.getTime()}ms`,
+                    },
+                    timestamp: new Date().toISOString()
+                }
+            ],
+        });
+        return NextResponse.json(
+            { success: false, error: "Could not find who ran the command" },
+            { status: 400 }
+        );
+    }
+
+    if (!interaction.member.roles.includes(IsleofDucks.roles.admin)) {
+        await FollowupMessage(interaction.token, {
+            embeds: [
+                {
+                    title: "Something went wrong!",
+                    description: "You don't have permission to use this command",
+                    color: IsleofDucks.colours.error,
+                    footer: {
+                        text: `Response time: ${Date.now() - timestamp.getTime()}ms`,
+                    },
+                    timestamp: new Date().toISOString()
+                }
+            ]
+        })
+        return NextResponse.json(
+            { success: false, error: "You don't have permission to use this command" },
+            { status: 403 }
+        );
+    }
+
+    replace = replace.toLowerCase().replaceAll(":", "");
+
+    if (/[^a-zA-Z]/.test(replace)) {
+        await FollowupMessage(interaction.token, {
+            embeds: [
+                {
+                    title: "Something went wrong!",
+                    description: "Invalid emoji name",
+                    color: IsleofDucks.colours.error,
+                    footer: {
+                        text: `Response time: ${Date.now() - timestamp.getTime()}ms`,
+                    },
+                    timestamp: new Date().toISOString()
+                }
+            ],
+        });
+        return NextResponse.json(
+            { success: false, error: "Invalid emoji name" },
+            { status: 400 }
+        );
+    }
+
+    const emojiExists = await checkEmoji(replace);
+    if (emojiExists) {
+        await FollowupMessage(interaction.token, {
+            embeds: [
+                {
+                    title: "Something went wrong!",
+                    description: "Emoji already exists",
+                    color: IsleofDucks.colours.error,
+                    footer: {
+                        text: `Response time: ${Date.now() - timestamp.getTime()}ms`,
+                    },
+                    timestamp: new Date().toISOString()
+                }
+            ],
+        });
+        return NextResponse.json(
+            { success: false, error: "Emoji already exists" },
+            { status: 400 }
+        );
+    }
+
+    await addEmoji(replace, withText);
+
+    await FollowupMessage(interaction.token, {
+        embeds: [
+            {
+                title: "Emoji added",
+                description: `Emoji added: :${replace}: -> ${withText}`,
+                color: IsleofDucks.colours.main,
+                footer: {
+                    text: `Response time: ${Date.now() - timestamp.getTime()}ms`,
+                },
+                timestamp: new Date().toISOString()
+            }
+        ],
+    });
+
+    return NextResponse.json(
+        { success: true },
+        { status: 200 }
+    );
+}
+async function removeChatEmoji(
+    interaction: APIChatInputApplicationCommandInteraction,
+    replace: string
+): Promise<
+    NextResponse<
+        {
+            success: boolean;
+            error?: string;
+        } | APIInteractionResponse
+    >
+> {
+    const timestamp = ConvertSnowflakeToDate(interaction.id);
+
+    if (!interaction.member) {
+        await FollowupMessage(interaction.token, {
+            embeds: [
+                {
+                    title: "Something went wrong!",
+                    description: "Could not find who ran the command",
+                    color: IsleofDucks.colours.error,
+                    footer: {
+                        text: `Response time: ${Date.now() - timestamp.getTime()}ms`,
+                    },
+                    timestamp: new Date().toISOString()
+                }
+            ],
+        });
+        return NextResponse.json(
+            { success: false, error: "Could not find who ran the command" },
+            { status: 400 }
+        );
+    }
+
+    if (!interaction.member.roles.includes(IsleofDucks.roles.admin)) {
+        await FollowupMessage(interaction.token, {
+            embeds: [
+                {
+                    title: "Something went wrong!",
+                    description: "You don't have permission to use this command",
+                    color: IsleofDucks.colours.error,
+                    footer: {
+                        text: `Response time: ${Date.now() - timestamp.getTime()}ms`,
+                    },
+                    timestamp: new Date().toISOString()
+                }
+            ]
+        })
+        return NextResponse.json(
+            { success: false, error: "You don't have permission to use this command" },
+            { status: 403 }
+        );
+    }
+
+    replace = replace.toLowerCase().replaceAll(":", "");
+
+    const emojiExists = await checkEmoji(replace);
+    if (!emojiExists) {
+        await FollowupMessage(interaction.token, {
+            embeds: [
+                {
+                    title: "Something went wrong!",
+                    description: "Emoji doesn't exists",
+                    color: IsleofDucks.colours.error,
+                    footer: {
+                        text: `Response time: ${Date.now() - timestamp.getTime()}ms`,
+                    },
+                    timestamp: new Date().toISOString()
+                }
+            ],
+        });
+        return NextResponse.json(
+            { success: false, error: "Emoji doesn't exists" },
+            { status: 400 }
+        );
+    }
+
+    await removeEmoji(replace);
+
+    await FollowupMessage(interaction.token, {
+        embeds: [
+            {
+                title: "Emoji removed",
+                description: `Emoji removed: ${replace}`,
+                color: IsleofDucks.colours.main,
+                footer: {
+                    text: `Response time: ${Date.now() - timestamp.getTime()}ms`,
+                },
+                timestamp: new Date().toISOString()
+            }
+        ],
+    });
+
+    return NextResponse.json(
+        { success: true },
+        { status: 200 }
+    );
+}
+async function viewChatEmojis(
+    interaction: APIChatInputApplicationCommandInteraction
+): Promise<
+    NextResponse<
+        {
+            success: boolean;
+            error?: string;
+        } | APIInteractionResponse
+    >
+> {
+    const timestamp = ConvertSnowflakeToDate(interaction.id);
+
+    if (!interaction.member) {
+        await FollowupMessage(interaction.token, {
+            embeds: [
+                {
+                    title: "Something went wrong!",
+                    description: "Could not find who ran the command",
+                    color: IsleofDucks.colours.error,
+                    footer: {
+                        text: `Response time: ${Date.now() - timestamp.getTime()}ms`,
+                    },
+                    timestamp: new Date().toISOString()
+                }
+            ],
+        });
+        return NextResponse.json(
+            { success: false, error: "Could not find who ran the command" },
+            { status: 400 }
+        );
+    }
+
+    if (!(
+        interaction.member.roles.includes(IsleofDucks.roles.admin) ||
+        interaction.member.roles.includes(IsleofDucks.roles.mod_duck) ||
+        interaction.member.roles.includes(IsleofDucks.roles.mod_duckling)
+    )) {
+        await FollowupMessage(interaction.token, {
+            embeds: [
+                {
+                    title: "Something went wrong!",
+                    description: "You don't have permission to use this command",
+                    color: IsleofDucks.colours.error,
+                    footer: {
+                        text: `Response time: ${Date.now() - timestamp.getTime()}ms`,
+                    },
+                    timestamp: new Date().toISOString()
+                }
+            ]
+        })
+        return NextResponse.json(
+            { success: false, error: "You don't have permission to use this command" },
+            { status: 403 }
+        );
+    }
+
+    const emojis = await getEmojis(0, 25);
+    const emojisCount = await getTotalEmojis();
+    if (!emojis.length || !emojisCount) {
+        await FollowupMessage(interaction.token, {
+            embeds: [
+                {
+                    title: "Something went wrong!",
+                    description: "Could not get emojis",
+                    color: IsleofDucks.colours.error,
+                    footer: {
+                        text: `Response time: ${Date.now() - timestamp.getTime()}ms`,
+                    },
+                    timestamp: new Date().toISOString()
+                }
+            ]
+        });
+        return NextResponse.json(
+            { success: false, error: "Could not get emojis" },
+            { status: 400 }
+        );
+    }
+    if (emojis.length === 0 || emojisCount === 0) {
+        await FollowupMessage(interaction.token, {
+            embeds: [
+                {
+                    title: "Something went wrong!",
+                    description: "No emojis found",
+                    color: IsleofDucks.colours.error,
+                    footer: {
+                        text: `Response time: ${Date.now() - timestamp.getTime()}ms`,
+                    },
+                    timestamp: new Date().toISOString()
+                }
+            ]
+        });
+        return NextResponse.json(
+            { success: false, error: "No emojis found" },
+            { status: 400 }
+        );
+    }
+
+    await FollowupMessage(interaction.token, {
+        embeds: [
+            {
+                title: "Emojis",
+                color: IsleofDucks.colours.main,
+                description: emojis.map(emoji => `"${emoji.replacetext}" -> "${emoji.withtext}"`).join("\n"),
+                footer: {
+                    text: `Response time: ${Date.now() - timestamp.getTime()}ms`
+                },
+                timestamp: new Date().toISOString()
+            }
+        ],
+        components: [
+            {
+                type: ComponentType.ActionRow,
+                components: [
+                    {
+                        custom_id: 'chatemoji-page_0',
+                        type: ComponentType.Button,
+                        label: '◀️',
+                        style: ButtonStyle.Primary,
+                        disabled: true
+                    },
+                    {
+                        custom_id: 'chatemoji-page_2',
+                        type: ComponentType.Button,
+                        label: '▶️',
+                        style: ButtonStyle.Primary,
+                        disabled: Math.ceil(emojisCount / 25) < 2
+                    }
+                ]
+            }
+        ]
+    });
+
+    return NextResponse.json(
+        { success: true },
+        { status: 200 }
+    );
+}
+
 export default async function(
     interaction: APIChatInputApplicationCommandInteraction
 ): Promise<
@@ -414,6 +765,10 @@ export default async function(
         if (options.filter.add) return await addFilter(interaction, options.filter.add.replace, options.filter.add.with);
         if (options.filter.remove) return await removeFilter(interaction, options.filter.remove.replace);
         if (options.filter.view) return await viewFilters(interaction);
+    } else if (options.emoji) {
+        if (options.emoji.add) return await addChatEmoji(interaction, options.emoji.add.replace, options.emoji.add.with);
+        if (options.emoji.remove) return await removeChatEmoji(interaction, options.emoji.remove.replace);
+        if (options.emoji.view) return await viewChatEmojis(interaction);
     }
     
     await FollowupMessage(interaction.token, {
@@ -478,6 +833,50 @@ export const CommandData = {
                 {
                     name: "view",
                     description: "View all chat filters on the bridge.",
+                    type: ApplicationCommandOptionType.Subcommand
+                }
+            ]
+        },
+        {
+            name: "emoji",
+            description: "Add or remove emojis to the bridge.",
+            type: ApplicationCommandOptionType.SubcommandGroup,
+            options: [
+                {
+                    name: "add",
+                    description: "Add an emoji to the bridge.",
+                    type: ApplicationCommandOptionType.Subcommand,
+                    options: [
+                        {
+                            name: "replace",
+                            description: "The text to replace.",
+                            type: ApplicationCommandOptionType.String,
+                            required: true
+                        },
+                        {
+                            name: "with",
+                            description: "The text to replace the replaced text with.",
+                            type: ApplicationCommandOptionType.String,
+                            required: true
+                        }
+                    ]
+                },
+                {
+                    name: "remove",
+                    description: "Remove an emoji from the bridge.",
+                    type: ApplicationCommandOptionType.Subcommand,
+                    options: [
+                        {
+                            name: "replace",
+                            description: "The emoji to remove.",
+                            type: ApplicationCommandOptionType.String,
+                            required: true
+                        }
+                    ]
+                },
+                {
+                    name: "view",
+                    description: "View all emojis on the bridge.",
                     type: ApplicationCommandOptionType.Subcommand
                 }
             ]
---
+++ src/discord/commands/component/chatemoji.ts
@@ -0,0 +1,117 @@
+import { ConvertSnowflakeToDate, CreateInteractionResponse, FollowupMessage, IsleofDucks } from "@/discord/discordUtils";
+import { getEmojis, getTotalEmojis } from "@/discord/utils";
+import { APIInteractionResponse, APIMessageComponentButtonInteraction, ButtonStyle, ComponentType, InteractionResponseType } from "discord-api-types/v10";
+import { NextResponse } from "next/server";
+
+async function viewEmojis(
+    interaction: APIMessageComponentButtonInteraction
+): Promise<
+    NextResponse<
+        {
+            success: boolean;
+            error?: string;
+        } | APIInteractionResponse
+    >
+> {
+    const timestamp = ConvertSnowflakeToDate(interaction.id);
+    
+    const page = interaction.data.custom_id.split("-")[1].split('_')[1];
+    const emojis = await getEmojis(( parseInt(page) - 1 ) * 25, 25);
+    const emojisCount = await getTotalEmojis();
+    if (!emojis.length || !emojisCount) {
+        await FollowupMessage(interaction.token, {
+            embeds: [
+                {
+                    title: "Something went wrong!",
+                    description: "Could not get emojis",
+                    color: IsleofDucks.colours.error,
+                    footer: {
+                        text: `Response time: ${Date.now() - timestamp.getTime()}ms`,
+                    },
+                    timestamp: new Date().toISOString()
+                }
+            ]
+        });
+        return NextResponse.json(
+            { success: false, error: "Could not get emojis" },
+            { status: 400 }
+        );
+    }
+    if (emojis.length === 0 || emojisCount === 0) {
+        await FollowupMessage(interaction.token, {
+            embeds: [
+                {
+                    title: "Something went wrong!",
+                    description: "No emojis found",
+                    color: IsleofDucks.colours.error,
+                    footer: {
+                        text: `Response time: ${Date.now() - timestamp.getTime()}ms`,
+                    },
+                    timestamp: new Date().toISOString()
+                }
+            ]
+        });
+        return NextResponse.json(
+            { success: false, error: "No emojis found" },
+            { status: 400 }
+        );
+    }
+
+    await FollowupMessage(interaction.token, {
+        embeds: [
+            {
+                title: "Emojis",
+                color: IsleofDucks.colours.main,
+                description: emojis.map(emoji => `"${emoji.replacetext}" -> "${emoji.withtext}"`).join("\n"),
+                footer: {
+                    text: `Response time: ${Date.now() - timestamp.getTime()}ms`,
+                },
+                timestamp: new Date().toISOString()
+            }
+        ],
+        components: [
+            {
+                type: ComponentType.ActionRow,
+                components: [
+                    {
+                        custom_id: `chateomji-page_${parseInt(page) - 1}`,
+                        type: ComponentType.Button,
+                        label: '◀️',
+                        style: ButtonStyle.Primary,
+                        disabled: page == '1'
+                    },
+                    {
+                        custom_id: `chateomji-page_${parseInt(page) + 1}`,
+                        type: ComponentType.Button,
+                        label: '▶️',
+                        style: ButtonStyle.Primary,
+                        disabled: Math.ceil(emojisCount / 25) < parseInt(page) + 1
+                    }
+                ]
+            }
+        ]
+    });
+
+    return NextResponse.json(
+        { success: true },
+        { status: 200 }
+    );
+}
+
+export default async function(
+    interaction: APIMessageComponentButtonInteraction
+): Promise<
+    NextResponse<
+        {
+            success: boolean;
+            error?: string;
+        } | APIInteractionResponse
+    >
+> {
+    // ACK response and update the original message
+    await CreateInteractionResponse(interaction.id, interaction.token, {
+        type: InteractionResponseType.DeferredMessageUpdate,
+    });
+
+    return await viewEmojis(interaction);
+}
