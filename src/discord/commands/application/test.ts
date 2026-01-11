2a09be9ed87e64cee9485ce8738e5a78bc9d067f
Jforjo <jforjoooooo13@gmail.com>
2026-01-11 17:02:06 +0000

    temp command to create initial survey responses thread

--- src/discord/commands/application/test.ts
+++ src/discord/commands/application/test.ts
@@ -1,19 +1,6 @@
-import { CreateInteractionResponse, FollowupMessage, IsleofDucks, SendMessage } from "@/discord/discordUtils";
-import { arrayChunks } from "@/discord/utils";
-import { SkyblockBazaarResponse } from "@zikeji/hypixel/dist/types/AugmentedTypes";
-import { APIChatInputApplicationCommandInteraction, APIInteractionResponse, ApplicationCommandType, ComponentType, InteractionResponseType, MessageFlags, RESTPatchAPIApplicationCommandJSONBody } from "discord-api-types/v10";
+import { CreateInteractionResponse, CreateThread, FollowupMessage, IsleofDucks } from "@/discord/discordUtils";
+import { APIChatInputApplicationCommandInteraction, APIInteractionResponse, ApplicationCommandType, InteractionResponseType, MessageFlags, RESTPatchAPIApplicationCommandJSONBody } from "discord-api-types/v10";
 import { NextResponse } from "next/server";
-
-async function getShards() {
-    const bzRes = await fetch("https://api.hypixel.net/v2/skyblock/bazaar");
-    if (!bzRes.ok) return;
-    const bzData = await bzRes.json() as SkyblockBazaarResponse;
-    if (!bzData.success) return;
-    return Object.entries(bzData.products)
-        .filter(([key,]) => key.startsWith("SHARD_"))
-        .sort(([, valueA], [, valueB]) => valueA.buy_summary[0].pricePerUnit - valueB.buy_summary[0].pricePerUnit)
-    ;
-}
 
 export default async function(
     interaction: APIChatInputApplicationCommandInteraction
@@ -25,36 +12,36 @@ export default async function(
         } | APIInteractionResponse
     >
 > {
-    // const member = interaction.member;
-    // if (!member) {
-    //     await CreateInteractionResponse(interaction.id, interaction.token, {
-    //         type: InteractionResponseType.ChannelMessageWithSource,
-    //         data: {
-    //             content: "Could not find who ran the command!",
-    //             flags: MessageFlags.Ephemeral
-    //         }
-    //     });
-    //     return NextResponse.json(
-    //         { success: false, error: "Could not find who ran the command" },
-    //         { status: 400 }
-    //     );
-    // }
-    // if (!(
-    //     member.user.id === IsleofDucks.staticIDs.Jforjo
-    //     // || member.roles.includes(IsleofDucks.roles.admin)
-    // )) {
-    //     await CreateInteractionResponse(interaction.id, interaction.token, {
-    //         type: InteractionResponseType.ChannelMessageWithSource,
-    //         data: {
-    //             content: "You do not have permission to run this command!",
-    //             flags: MessageFlags.Ephemeral
-    //         }
-    //     });
-    //     return NextResponse.json(
-    //         { success: false, error: "You do not have permission to run this command" },
-    //         { status: 400 }
-    //     );
-    // }
+    const member = interaction.member;
+    if (!member) {
+        await CreateInteractionResponse(interaction.id, interaction.token, {
+            type: InteractionResponseType.ChannelMessageWithSource,
+            data: {
+                content: "Could not find who ran the command!",
+                flags: MessageFlags.Ephemeral
+            }
+        });
+        return NextResponse.json(
+            { success: false, error: "Could not find who ran the command" },
+            { status: 400 }
+        );
+    }
+    if (!(
+        member.user.id === IsleofDucks.staticIDs.Jforjo
+        // || member.roles.includes(IsleofDucks.roles.admin)
+    )) {
+        await CreateInteractionResponse(interaction.id, interaction.token, {
+            type: InteractionResponseType.ChannelMessageWithSource,
+            data: {
+                content: "You do not have permission to run this command!",
+                flags: MessageFlags.Ephemeral
+            }
+        });
+        return NextResponse.json(
+            { success: false, error: "You do not have permission to run this command" },
+            { status: 400 }
+        );
+    }
 
     await CreateInteractionResponse(interaction.id, interaction.token, {
         type: InteractionResponseType.DeferredChannelMessageWithSource,
@@ -62,33 +49,6 @@ export default async function(
             flags: MessageFlags.Ephemeral
         }
     });
-
-    const bzData = await getShards();
-    if (!bzData) {
-        await FollowupMessage(interaction.token, {
-            content: "Failed to fetch shards"
-        });
-        return NextResponse.json(
-            { success: false, error: "Failed to fetch shards" },
-            { status: 400 }
-        );
-    }
-
-    for (const chunk of arrayChunks(bzData, 39)) {
-        await SendMessage(interaction.channel.id, {
-            flags: MessageFlags.IsComponentsV2,
-            components: [
-                {
-                    type: ComponentType.Container,
-                    accent_color: IsleofDucks.colours.main,
-                    components: chunk.map(([key, value]) => ({
-                        type: ComponentType.TextDisplay,
-                        content: `**${key}** - ${Number(value.quick_status.sellPrice).toLocaleString("en-US", { maximumFractionDigits: 2, minimumFractionDigits: 0 })} / ${Number(value.quick_status.buyPrice).toLocaleString("en-US", { maximumFractionDigits: 2, minimumFractionDigits: 0 })}`,
-                    }))
-                }
-            ]
-        })
-    }
 
     // await SendMessage(interaction.channel.id, {
     //     flags: MessageFlags.IsComponentsV2,
@@ -136,6 +96,26 @@ export default async function(
     //         flags: MessageFlags.Ephemeral
     //     }
     // });
+
+    await CreateThread("1459947888003125384", {
+        name: "Survey Responses",
+        message: {
+            content: "**Total Survey Responses**\n0 Guild App",
+            embeds: [
+                {
+                    title: "Guild App",
+                    description: "Survey presented after players open a guild application ticket",
+                    color: 16489216,
+                    fields: [
+                        {
+                            name: "Where did you find our guild?",
+                            value: "0 SBZ/SBS Discord\n0 Forums\n0 On Hypixel\n0 A Friend\n0 I'm a Returning Member\n0 Other",
+                        },
+                    ],
+                },
+            ],
+        }
+    });
 
     await FollowupMessage(interaction.token, {
         content: `Done!`,
