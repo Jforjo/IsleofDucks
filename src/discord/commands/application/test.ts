7fe4c45f199e6baf0a00a4781ea67279a90107d0
Jforjo <jforjoooooo13@gmail.com>
2026-03-28 10:34:23 +0000

    feat: add guilds fetching functionality to the test command

--- src/discord/commands/application/test.ts
+++ src/discord/commands/application/test.ts
@@ -1,6 +1,6 @@
 import { CreateInteractionResponse, CreateThread, FollowupMessage, getUserDetails, IsleofDucks } from "@/discord/discordUtils";
 import { getUserDataFromDiscordID } from "@/discord/utils";
-import { APIChatInputApplicationCommandInteraction, APIInteractionResponse, ApplicationCommandType, ButtonStyle, ComponentType, InteractionResponseType, MessageFlags } from "discord-api-types/v10";
+import { APIChatInputApplicationCommandInteraction, APIGuild, APIInteractionResponse, ApplicationCommandType, ButtonStyle, ComponentType, InteractionResponseType, MessageFlags } from "discord-api-types/v10";
 import { NextResponse } from "next/server";
 
 export default async function(
@@ -51,45 +51,53 @@ export default async function(
         }
     });
 
-    const userDataRes = await getUserDataFromDiscordID(member.user.id);
-    if (!userDataRes.success) {
-        await FollowupMessage(interaction.token, {
-            content: "Could not find user in DB!",
-        });
-        return NextResponse.json(
-            { success: false, error: "Could not find user in DB" },
-            { status: 400 }
-        );
-    }
-    console.log("userDataRes", JSON.stringify(userDataRes, null, 2));
-    const user = await getUserDetails(userDataRes.data.discord.accesstoken, userDataRes.data.discord.refreshtoken);
-    if (!user) {
-        await FollowupMessage(interaction.token, {
-            content: "Could not find user!",
-        });
-        return NextResponse.json(
-            { success: false, error: "Could not find user" },
-            { status: 400 }
-        );
-    }
+    // const userDataRes = await getUserDataFromDiscordID(member.user.id);
+    // if (!userDataRes.success) {
+    //     await FollowupMessage(interaction.token, {
+    //         content: "Could not find user in DB!",
+    //     });
+    //     return NextResponse.json(
+    //         { success: false, error: "Could not find user in DB" },
+    //         { status: 400 }
+    //     );
+    // }
+    // console.log("userDataRes", JSON.stringify(userDataRes, null, 2));
+    // const user = await getUserDetails(userDataRes.data.discord.accesstoken, userDataRes.data.discord.refreshtoken);
+    // if (!user) {
+    //     await FollowupMessage(interaction.token, {
+    //         content: "Could not find user!",
+    //     });
+    //     return NextResponse.json(
+    //         { success: false, error: "Could not find user" },
+    //         { status: 400 }
+    //     );
+    // }
 
-    console.log("user", JSON.stringify(user, null, 2));
-    await FollowupMessage(interaction.token, {
-        content: `\`\`\`${JSON.stringify(user, null, 2)}\`\`\``,
-        components: [
-            {
-                type: ComponentType.ActionRow,
-                components: [
-                    {
-                        type: ComponentType.Button,
-                        style: ButtonStyle.Link,
-                        label: "Try reauthenticating",
-                        url: "https://isle-of-ducks.vercel.app/api/auth/discord/redirect"
-                    }
-                ]
-            }
-        ]
+    // console.log("user", JSON.stringify(user, null, 2));
+    // await FollowupMessage(interaction.token, {
+    //     content: `\`\`\`${JSON.stringify(user, null, 2)}\`\`\``,
+    //     components: [
+    //         {
+    //             type: ComponentType.ActionRow,
+    //             components: [
+    //                 {
+    //                     type: ComponentType.Button,
+    //                     style: ButtonStyle.Link,
+    //                     label: "Try reauthenticating",
+    //                     url: "https://isle-of-ducks.vercel.app/api/auth/discord/redirect"
+    //                 }
+    //             ]
+    //         }
+    //     ]
+    // });
+
+    const res = await fetch("https://discord.com/api/v10/users/@me/guilds", {
+        headers: {
+            Authorization: `Bearer ${process.env.DISCORD_BOT_TOKEN}`
+        }
     });
+    const guilds = await res.json() as APIGuild[];
+    console.log("guilds", JSON.stringify(guilds, null, 2));
 
     return NextResponse.json(
         { success: true },
