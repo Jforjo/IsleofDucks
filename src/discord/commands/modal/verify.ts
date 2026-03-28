dab5600a11e2158eebac61aaf51cd142561323c4
Jforjo <jforjoooooo13@gmail.com>
2026-03-28 09:49:55 +0000

    fix: enhance Discord account verification process with improved error handling and user feedback

--- src/discord/commands/modal/verify.ts
+++ src/discord/commands/modal/verify.ts
@@ -1,7 +1,7 @@
 import { AddGuildMemberRole, CreateInteractionResponse, FollowupMessage, IsleofDucks } from "@/discord/discordUtils";
-import { getUsernameOrUUID } from "@/discord/hypixelUtils";
+import { getHypixelPlayer, getUsernameOrUUID } from "@/discord/hypixelUtils";
 import { checkDiscordInDB, checkLinked, checkMinecraftInDB, createMinecraftUser, linkDiscordToMinecraft } from "@/discord/utils";
-import { APIModalSubmitInteraction, APIInteractionResponse, Snowflake, InteractionResponseType, MessageFlags, ComponentType, ButtonStyle } from "discord-api-types/v10";
+import { APIModalSubmitInteraction, APIInteractionResponse, Snowflake, InteractionResponseType, MessageFlags, ComponentType, ButtonStyle, APIUser } from "discord-api-types/v10";
 import { NextResponse } from "next/server";
 
 export default async function(
@@ -18,9 +18,9 @@ export default async function(
         type: InteractionResponseType.DeferredChannelMessageWithSource,
         data: { flags: MessageFlags.Ephemeral }
     });
-    let userId: Snowflake;
-    if (interaction.member) userId = interaction.member.user.id;
-    else if (interaction.user) userId = interaction.user.id;
+    let user: APIUser;
+    if (interaction.member) user = interaction.member.user;
+    else if (interaction.user) user = interaction.user;
     else {
         await FollowupMessage(interaction.token, {
             content: "Could not find user ID from interaction!" ,
@@ -64,7 +64,38 @@ export default async function(
         );
     }
 
-    const discordExists = await checkDiscordInDB(userId);
+    const discordHypixelRes = await getHypixelPlayer(userRes.uuid);
+    if (!discordHypixelRes.success) {
+        await FollowupMessage(interaction.token, {
+            content: discordHypixelRes.message || "An error occurred while fetching your Hypixel data!",
+        });
+        return NextResponse.json(
+            { success: false, error: discordHypixelRes.message || "An error occurred while fetching your Hypixel data" },
+            { status: 400 }
+        );
+    }
+
+    if (!discordHypixelRes.player.socialMedia || !discordHypixelRes.player.socialMedia.links || !discordHypixelRes.player.socialMedia.links.DISCORD) {
+        await FollowupMessage(interaction.token, {
+            content: "You must link your Discord account on Hypixel!",
+        });
+        return NextResponse.json(
+            { success: false, error: "You must link your Discord account on Hypixel" },
+            { status: 400 }
+        );
+    }
+    const linkedDiscord = discordHypixelRes.player.socialMedia.links.DISCORD;
+    if (linkedDiscord !== user.username) {
+        await FollowupMessage(interaction.token, {
+            content: `The Discord account linked on your Hypixel profile (${linkedDiscord}) does not match the Discord account you used to verify (${user.username})!`,
+        });
+        return NextResponse.json(
+            { success: false, error: "The Discord account linked on your Hypixel profile does not match the Discord account you used to verify" },
+            { status: 400 }
+        );
+    }
+
+    const discordExists = await checkDiscordInDB(user.id);
     if (!discordExists) {
         await FollowupMessage(interaction.token, {
             flags: MessageFlags.Ephemeral | MessageFlags.IsComponentsV2,
@@ -98,7 +129,7 @@ export default async function(
     }
 
     try {
-        await linkDiscordToMinecraft(userId, userRes.uuid);
+        await linkDiscordToMinecraft(user.id, userRes.uuid);
     } catch (e) {
         await FollowupMessage(interaction.token, {
             content: "An error occurred while linking your Discord account to your Minecraft account!"
@@ -106,7 +137,7 @@ export default async function(
         throw e;
     }
 
-    const didLink = await checkLinked(userId, userRes.uuid);
+    const didLink = await checkLinked(user.id, userRes.uuid);
     if (!didLink) {
         await FollowupMessage(interaction.token, {
             content: "An error occurred while linking your Discord account to your Minecraft account!"
@@ -118,7 +149,7 @@ export default async function(
     }
 
     try {
-        await AddGuildMemberRole(IsleofDucks.serverID, userId, "1486811494695043182");
+        await AddGuildMemberRole(IsleofDucks.serverID, user.id, "1486811494695043182");
     } catch (e) {
         await FollowupMessage(interaction.token, {
             content: "Successfully linked your Discord account to your Minecraft account, but failed to add the verified role!\nPlease contact our staff to resolve this issue.",
