f85f33b280328dde8cf863a0be64c264cd9e72d4
Jforjo <jforjoooooo13@gmail.com>
2026-04-18 12:45:53 +0100

    feat: add check for superlative last updated in user retrieval process

--- src/discord/commands/application/test.ts
+++ src/discord/commands/application/test.ts
@@ -99,6 +99,7 @@ export default async function(
     const users = await getAllMinecraftUsers();
     for (const user of users) {
         if (user.exp !== 0) continue;
+        if (user.superlativelastupdated !== 0) continue;
         const hypixel = await checkPlayer(user.uuid);
         if (!hypixel.success) {
             if (hypixel.message === "Key throttle") return NextResponse.json(
