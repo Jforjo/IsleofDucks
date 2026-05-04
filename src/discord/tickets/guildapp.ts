8d166b608cb90a64394d4ffcd502e6816a2abc29
Jforjo <jforjoooooo13@gmail.com>
2026-05-04 15:46:20 +0100

    refactor: enhance guild application to include username retrieval from user data

--- src/discord/tickets/guildapp.ts
+++ src/discord/tickets/guildapp.ts
@@ -1,7 +1,8 @@
 import { APIInteractionResponse, APIMessageComponentButtonInteraction, ComponentType, InteractionResponseType, TextInputStyle } from "discord-api-types/v10";
 import { CreateInteractionResponse } from "@/discord/discordUtils";
 import { NextResponse } from "next/server";
-import { getSettingValue } from "../utils";
+import { getSettingValue, getUserDataFromDiscordID } from "../utils";
+import { getUsernameOrUUID } from "../hypixelUtils";
 
 export default async function(
     interaction: APIMessageComponentButtonInteraction
@@ -13,13 +14,23 @@ export default async function(
         } | APIInteractionResponse
     >
 > {
+    const userData = await getUserDataFromDiscordID(interaction.member!.user.id);
+
     const duckReq = await getSettingValue("duck_req");
     const ducklingReq = await getSettingValue("duckling_req");
+
+    let username = undefined;
+    if (userData?.success && userData.data?.minecraft?.uuid) {
+        const mojang = await getUsernameOrUUID(userData.data.minecraft.uuid);
+        if (mojang.success) {
+            username = mojang.name;
+        }
+    }
 
     await CreateInteractionResponse(interaction.id, interaction.token, {
         type: InteractionResponseType.Modal,
         data: {
-            custom_id: "guildapp",
+            custom_id: `guildapp${userData?.success && userData.data?.minecraft?.uuid ? `-${userData.data.minecraft.uuid}` : ""}`,
             title: "Guild Application",
             components: [
                 {
@@ -49,14 +60,15 @@ export default async function(
                     component: {
                         type: ComponentType.TextInput,
                         custom_id: "username",
+                        value: username,
                         placeholder: "Enter your Minecraft username",
                         style: TextInputStyle.Short,
                         min_length: 3,
                         max_length: 16,
                         required: true,
                     },
-                },
-            ],
+                }
+            ]
         }
     });
 
