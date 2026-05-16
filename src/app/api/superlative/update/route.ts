b562b9ab2e8276b763fd3ca1bf9ec891bf3d3b8e
Jforjo <jforjoooooo13@gmail.com>
2026-05-16 15:35:40 +0100

    feat: implement GET endpoint for updating superlative ranks based on UUID and guild

---
+++ src/app/api/superlative/update/route.ts
@@ -0,0 +1,78 @@
+import { formatNumber, getSuperlativeValue, IsleofDucks, SendMessage } from "@/discord/discordUtils";
+import { getActiveSuperlative } from "@/discord/utils";
+import type { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest): Promise<Response> {
+    const authHeader = request.headers.get("authorization");
+    if (!authHeader?.includes('Bearer ')) {
+        return Response.json({
+            success: false,
+            message: "Missing authorization header"
+        });
+    }
+    const APIKey = authHeader.split(' ')[1];
+    if (!process.env.BRIDGE_API_KEY) throw new Error('BRIDGE_API_KEY is not defined');
+    if (APIKey !== process.env.BRIDGE_API_KEY) {
+        return Response.json({
+            success: false,
+            message: "Invalid API key"
+        });
+    }
+
+    const params = request.nextUrl.searchParams;
+    const uuid = params.get("uuid");
+    const guild = params.get("guild");
+
+    const superlative = await getActiveSuperlative();
+    if (!superlative) {
+        return Response.json({
+            success: false,
+            message: "No active superlative found"
+        });
+    }
+
+    if (uuid) {
+        const superlativeData = await getSuperlativeValue(uuid, (value) => formatNumber(value, superlative.dp));
+        if (!superlativeData.success) {
+            return Response.json({
+                success: false,
+                message: superlativeData.message
+            });
+        }
+
+        let rankShould = "";
+        if (guild === "duck") {
+            superlative.duckranks.sort((a, b) => a.requirement - b.requirement).forEach((rank, _index) => {
+                if (rank.requirement <= superlativeData.current) {
+                    rankShould = rank.name.toLowerCase();
+                }
+            });
+            await SendMessage(IsleofDucks.channels.duckoc, {
+                content: `setrank ${uuid} ${rankShould}`
+            });
+        } else if (guild === "duckling") {
+            superlative.ducklingranks.sort((a, b) => a.requirement - b.requirement).forEach((rank, _index) => {
+                if (rank.requirement <= superlativeData.current) {
+                    rankShould = rank.name.toLowerCase();
+                }
+            });
+            await SendMessage(IsleofDucks.channels.ducklingoc, {
+                content: `setrank ${uuid} ${rankShould}`
+            });
+        } else {
+            return Response.json({
+                success: false,
+                message: "Missing or invalid guild parameter"
+            });
+        }
+    } else {
+        return Response.json({
+            success: false,
+            message: "Missing uuid or guild parameter"
+        });
+    }
+
+    return Response.json({
+        success: true
+    });
+}
